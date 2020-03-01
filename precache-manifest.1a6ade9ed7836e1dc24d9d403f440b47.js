22cf0797ff50f7f395871551987183767d8105d7
manassuri7 <manassuri7@outlook.com>
2020-03-01 17:53:45 +0530

    Updates

---
+++ precache-manifest.1a6ade9ed7836e1dc24d9d403f440b47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49595aa8477fd23a9bd16a90d146b450",
+    "url": "/table-list/index.html"
+  },
+  {
+    "revision": "236638125e5de6617fd1",
+    "url": "/table-list/static/css/main.ea157d10.chunk.css"
+  },
+  {
+    "revision": "69d84294ecab0425dcf1",
+    "url": "/table-list/static/js/2.5af73900.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/table-list/static/js/2.5af73900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236638125e5de6617fd1",
+    "url": "/table-list/static/js/main.baec87c4.chunk.js"
+  },
+  {
+    "revision": "742cdd37096c48e04d98",
+    "url": "/table-list/static/js/runtime-main.87f47210.js"
+  }
+]);
